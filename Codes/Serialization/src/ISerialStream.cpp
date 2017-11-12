af54185fc561e9ab2618efce0eb2daa1d71df1af
yingxd <yingxd@yingxd-PC>
2017-11-12 22:15:37 +0800

    移动过时的xml代码。

--- Codes/Serialization/src/ISerialStream.cpp
+++ Codes/Serialization/src/ISerialStream.cpp
@@ -2,7 +2,7 @@
 #include "..\include\ISerialStream.h"
 
 #include "..\include\SerialStreamBinary.h"
-#include "..\include\SerialStreamXml.h"
+//#include "..\include\SerialStreamXml.h"
 
 ISerialStream *ISerialStream::CreateSerialStreamIntf(ESerialStreamIntfType IntfType, std::iostream *pStream, bool hold_stream)
 {
--- Codes/src/MetaDataObjects.cpp
+++ Codes/src/MetaDataObjects.cpp
@@ -2,6 +2,9 @@
 
 #include "..\MetaData\include\MetaDataSupport2.h"
 
+/*案例*/
+/*要使用MetaData.dll的，都要在这个目录位置建立一个MetaDataObject.cpp文件！
+以下的内容，根据不同项目，内容是各不相同的。*/
 ////////////////////////////////////////////////
 //             Extra Meta Data                //
 ////////////////////////////////////////////////
