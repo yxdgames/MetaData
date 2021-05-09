9c0647aa82715f408826f42823c173b90d97d06d
yingxd <yingxd@ROG.MYPC>
2021-05-09 21:35:18 +0800

    TArray更新.

--- Codes/include/TArray.h
+++ Codes/include/TArray.h
@@ -6,7 +6,10 @@ template <typename T>
 class TEMPLATE_CLASS_DESCRIPT TArray
 {
 public:
+	TArray(void) : m_pArray(nullptr), m_Size(0) {}
 	inline TArray(const size_t size);
+	inline TArray(T *&&array, const size_t size);
+	inline TArray(TArray &&src);
 	inline ~TArray(void);
 public:
 	void Reset(const size_t size, bool allow_greater = false);
@@ -16,8 +19,8 @@ public:
 public:
 	inline T &operator[](size_t index);
 	inline TArray &operator=(TArray &src);
+	inline TArray &operator=(TArray &&src);
 private:
-	TArray(void) {}
 	TArray(TArray&) {}
 private:
 	T *m_pArray;
@@ -26,8 +29,23 @@ private:
 
 template <typename T>
 inline TArray<T>::TArray(const size_t size)
-	:m_pArray(size ? new T[size] : nullptr), m_Size(size)
+	: m_pArray(size ? new T[size] : nullptr), m_Size(size)
 {
+}
+
+template <typename T>
+inline TArray<T>::TArray(T *&&array, const size_t size)
+	: m_pArray(array), m_Size(size)
+{
+	array = nullptr;
+}
+
+template <typename T>
+inline TArray<T>::TArray(TArray &&src)
+	: m_pArray(src.m_pArray), m_Size(src.m_Size)
+{
+	src.m_pArray = nullptr;
+	src.m_Size = 0;
 }
 
 template <typename T>
@@ -63,6 +81,17 @@ inline TArray<T> &TArray<T>::operator=(TArray<T> &src)
 {
 	this->Reset(src.array_size());
 	memcpy(this->array(), src.array(), this->array_size());
+	return *this;
+}
+
+template <typename T>
+inline TArray<T> &TArray<T>::operator=(TArray &&src)
+{
+	if (m_pArray) delete[] m_pArray;
+	this->m_pArray = src.m_pArray;
+	this->m_Size = src.m_Size;
+	src.m_pArray = nullptr;
+	src.m_Size = 0;
 	return *this;
 }
 
